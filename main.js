8aa3d9cdc2c866c468b5aeafdd44046c19a1e515
Rolo115 <113950518+Rolo115@users.noreply.github.com>
2022-11-11 18:59:19 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
 
-function login(){
-   let username = document.getElementById('username').value
-   let  password = document.getElementById('password').value
-console.log( username);
-console.log(password);
-    if(username == "Ash Ketchum" && password == "1234"){
-    window.location= "pantalla de accion.html";
+
+const user_cuenta = document.getElementById('usercuenta')
+let saldo = 500
+
+function login(){ 
+    var user = document.getElementById('username').value
+   var pass = document.getElementById('password').value     
+    const datos = {user,
+    pass, saldo
+    };        
+
+    if(user == "Ash Ketchum" && pass == "1234"){
+        console.log(datos); 
+        localStorage.setItem('saldo',500);
+         if(localStorage.getItem('infocuenta')){
+        user_cuenta.innerText = localStorage.getItem('infocuenta')
+        }
+        else{
+        localStorage.setItem('infocuenta',JSON.stringify(user));
+        window.location= "pantalla de accion.html";  
+        }
+        }
+    else{
+        swal ( "Oops" ,  "No se ingreso ningún valor ó la contraseña, usuario son incorrectos",  "error");
+        }
+    if(user == "Ash Ketchum" && pass == ""){
+        swal ( "Oops" ,  "No se ingreso la contraseña!",  "error");
+        
+        }
+        if(user == "" && pass == "1234"){
+            swal ( "Oops" ,  "No se ingreso el usuario!",  "error");
+            }
 }
-}
+    window.onload = function imprimir(){
+        user_cuenta.innerText = localStorage.getItem('infocuenta');
+    }
